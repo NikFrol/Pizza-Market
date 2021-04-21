e5999cba6052ca685c2748c4c8f660d92010a26b
Frol Nikitich <nf1694@gmail.com>
2021-04-21 23:18:30 +0300

    Some thing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import React from 'react';
+
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 
@@ -8,42 +8,33 @@ import { Home, Cart } from './routes';
 import notFound404 from './assets/notFound404.png';
 
 const App = () => {
-  return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-      <Switch>
-                <Route path='/404' render={() => (
-                    <>
-                    <h1>Not Found, go back</h1>
-                    <img width="100%" src={notFound404} alt="404" />
-                    </>)}
-                />
-                <Route>
-                    <>
-                        
-                            <Switch>
-                                <Route path='/' exact component={Home} />
-                                <Route path='/cart' component={Cart} />
-                                <Route path='/about' render={() => (
-                                    <h1>About</h1>
-                                )} />
-                                <Route path='/contact' render={() => (
-                                    <h1>Contacts</h1>
-                                )} />
-                                <Route render={() => (
-                                    <Redirect to={'/404'} />
-                                )
-                                } />
-                            </Switch>
-                       
-                     
-                    </>
-                </Route>
-            </Switch>
-      </div>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <Switch>
+                    <Route path='/404' render={() => (
+                        <>
+                            <h1>Not Found, go back</h1>
+                            <img width="100%" src={notFound404} alt="404" />
+                        </>)}
+                    />
+                    <Route>
+                        <Switch>
+                            <Route path='/' exact component={Home} />
+                            <Route path='/cart' component={Cart} />
+                            <Route path='/about' render={() => (
+                                <h1>About</h1>)} />
+                            <Route path='/contact' render={() => (
+                                <h1>Contacts</h1>)} />
+                            <Route render={() => (
+                                <Redirect to={'/404'} />)} />
+                        </Switch>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    );
 }
 
 export default App;
